ce7475718e4b20c4152ef77a458e4876bd7369b2
1z3r091 <1z3r091@gmail.com>
2022-11-21 11:42:59 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Editttttt <code>src/App.tsx</code> and save to test HMR
+          Go to <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
