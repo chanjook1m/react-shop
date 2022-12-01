10d34d942caa24b30cc03a8b26328bccd16b4b28
1z3r091 <1z3r091@gmail.com>
2022-12-01 17:52:47 +0900

    feat/헤더 스타일 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,7 @@
 import { useState, useMemo, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { debounce } from "lodash";
-
+import { FaRegMoon, FaRegSun } from "react-icons/fa";
 interface Categories {
   path: string;
   name: string;
@@ -28,6 +28,7 @@ const API_URL = "https://fakestoreapi.com/products";
 export default function Header(props: HeaderProps) {
   const [data, setData] = useState<ProductInfo[]>([]);
   const [query, setQuery] = useState<string>("");
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -50,13 +51,15 @@ export default function Header(props: HeaderProps) {
   );
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.value) debouncedSearch(e.target.value);
-    else setData([]);
+    if (e.target.value) {
+      debouncedSearch(e.target.value);
+      setShow(true);
+    } else setData([]);
     console.log(data);
   };
 
   return (
-    <header className="w-full fixed top-0 left-0 h-16 shadow z-10 bg-white">
+    <header className="w-full fixed top-0 left-0 h-16 shadow z-10 bg-white font-bold text-black">
       <div className="w-11/12 h-full flex max-w-screen-xl justify-between my-0 mx-auto items-center">
         <div className="header-left flex space-x-6">
           <div>
@@ -81,14 +84,22 @@ export default function Header(props: HeaderProps) {
           </nav>
         </div>
         <div className="header-right">
-          <ul className="flex space-x-4">
-            <li>dark</li>
+          <ul className="flex space-x-4 items-center">
+            <li>
+              <button>
+                <FaRegMoon />
+              </button>
+            </li>
             <li>
               <input
                 type="text"
                 placeholder="검색"
                 onChange={onChange}
+                onBlur={(e) => {
+                  setTimeout(() => setShow(false), 100);
+                }}
                 ref={inputRef}
+                className="outline-none bg-gray-100 p-3 rounded"
               />
               <div
                 style={{
@@ -99,7 +110,8 @@ export default function Header(props: HeaderProps) {
                 }}
               >
                 <ul className="flex flex-col">
-                  {data &&
+                  {show &&
+                    data &&
                     data.map(
                       (ele) =>
                         ele.title.indexOf(query) >= 0 && (
@@ -109,7 +121,6 @@ export default function Header(props: HeaderProps) {
                           >
                             <a
                               onClick={(e) => {
-                                e.preventDefault();
                                 if (inputRef.current)
                                   inputRef.current.value = "";
                                 setQuery("");
